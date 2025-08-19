8d62d8e979358742b9ee8e1bb5226ff444e53bcf
urmee04 <urmee04sust@gmail.com>
2025-08-19 15:27:28 -0400

    craete an API route and fetch from external API

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 //import the express library
-const express = require(express);
-
+const express = require("express");
+//import axios library
+const axios = require("axios");
 //create an instance of an express application
-const app = Express();
+const app = express();
 
 //define the port to run the server
 const port = 3000;
+
+//create a new GET route at the path /api/fun-fact
+
+app.get("/api/fun-fact", async (req, res) => {
+  try {
+    const response = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random");
+
+    const funFact = {
+      fact: response.data.text,
+    };
+
+    res.json(funFact);
+  } catch (error) {
+    console.error("Error Fetching Facts:", error.message);
+    res.status(500).json({ error: "Could not fetch fun fact" });
+  }
+});
+
 
 //start the server on the port 3000;
 app.listen(port, () => {
